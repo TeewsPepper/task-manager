66f135e7c3e2579982981cc29d1c3669e6e9ff26
TeewsPepper <gomez.pepper@gmail.com>
2025-06-30 19:15:33 -0300

    update config

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,25 +1,28 @@
 // src/app.ts
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import taskRoutes from './routes/tasks';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import taskRoutes from "./routes/tasks";
 
 dotenv.config();
 
 const app = express();
 
 // Middleware
-app.use(cors({
-  origin: [
-    "https://task-manager-1-vk2h.onrender.com", // URL de tu frontend en Render
-    "http://localhost:5173"                     // Desarrollo local
-  ],
-  credentials: true
-}));
+app.use(
+  cors({
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    origin: [
+      "https://task-manager-1-vk2h.onrender.com", // URL de tu frontend en Render
+      "http://localhost:5173", // Desarrollo local
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // Rutas
-app.use('/api', taskRoutes);
+app.use("/api", taskRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
